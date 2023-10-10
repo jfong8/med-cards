1cd1765ce4e00fb403f54a92b9245d3f78635da3
Telmen Bayarsaikhan <telmen.bayarsaikhan@motorolasolutions.com>
2023-10-09 19:59:46 -0500

    add uuid to temp cards objects

--- frontend/src/components/CardGrid.jsx
+++ frontend/src/components/CardGrid.jsx
@@ -2,19 +2,23 @@ import * as React from 'react';
 import {
   Grid, Card, CardActionArea, CardMedia, CardContent, Typography,
 } from '@mui/material';
+import { v4 as uuidv4 } from 'uuid';
 
 const cardInfo = [
   {
+    id: uuidv4(),
     name: 'Femur',
     description: 'The bone of the thigh or upper hind limb, articulating at the hip and the knee',
     image: 'https://cdn.britannica.com/92/99192-050-52E7AB99/view-femur.jpg',
   },
   {
+    id: uuidv4(),
     name: 'Tibia',
     description: 'The tibia, also known as the shinbone or shankbone, is the larger, stronger, and anterior of the two bones in the leg below the knee in vertebrates; it connects the knee with the ankle',
     image: 'https://cdn.britannica.com/05/99105-120-6738BAE9/view-bones-leg-tibia-fibula.jpg',
   },
   {
+    id: uuidv4(),
     name: 'Scapula',
     description: 'The scapula, also known as the shoulder blade, is the bone that connects the humerus with the clavicle.',
     image: 'https://cdn.britannica.com/06/99106-050-CE6AFD10/view-bones-shoulder-humerus-scapula-clavicle.jpg',
