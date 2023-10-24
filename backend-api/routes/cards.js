e8c13c68188e8f7906dfc706e34af28c995b1d49
Telmen Bayarsaikhan <telmen.bayarsaikhan@motorolasolutions.com>
2023-10-23 23:38:40 -0500

    cleanup and use descriptive descriptions

--- backend-api/routes/cards.js
+++ backend-api/routes/cards.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const bonesData = require('../data/bones.json');
+const bonesData = require('../data/bones-description.json');
 
 // Triggered when request is made to 'api/cards/'
 router.get('/', (req, res) => {
